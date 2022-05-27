f67e40e67420677d54cdb2d41f55ebc6810a21b5
qwa-crysthianpires <crysthian.pires@qwasolucoes.com.br>
2022-05-27 14:23:34 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,16 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		Board board = new Board (8,8);
-		
-		System.out.println(pos);
-		System.out.println(board);
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 	}
 
 }
+
+
+
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -16,27 +16,31 @@ public class Board {
 	}
 
 
-
+	//geteset_row
 	public int getRows() {
 		return rows;
 	}
 
-
-
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
-
-
-
+	
+	//geteset_column
 	public int getColumns() {
 		return columns;
 	}
 
-
-
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-
+	
+	
+	//METODOS
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
